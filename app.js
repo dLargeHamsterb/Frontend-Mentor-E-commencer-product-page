9f50513faf5a73dc00f56ebb33d5199a3d861e68
dLargeHamsterb <pollo0@o2.pl>
2022-07-19 22:20:00 +0200

    Change product display, add json file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+import {products, displayProducts} from './products.js'
+
+const productList = document.querySelector(".products-list")
+productList.innerHTML=displayProducts(products)
+
+const eachProduct = document.querySelectorAll(".product")
+
 const shoppingCart = document.querySelector(".shopping-cart")
 const cartContainer = document.querySelector(".cart-img")
 
-const productImg = document.querySelector(".product-img")
+const productImg = document.querySelectorAll(".product-img")
 // const thumbnails = document.querySelectorAll(".thumbnails")
 const thumbnailsImg = document.querySelectorAll(".thumbnail-img")
 
@@ -9,163 +16,189 @@ const galleryContainer = document.querySelector(".wrapper")
 const galleryImg = document.querySelector(".gallery-img")
 const galleryThumbnails= document.querySelectorAll(".gallery-thumbnails .thumbnail .thumbnail-img")
 
-const closeBtn = document.querySelector(".close-btn")
-const nextBtn = document.querySelector(".next-btn")
-const previusBtn = document.querySelector(".previus-btn")
+const closeBtn = document.querySelectorAll(".close-btn")
+const nextBtn = document.querySelectorAll(".next-btn")
+const previusBtn = document.querySelectorAll(".previus-btn")
 
-const increase = document.querySelector(".increase")
-const decrease = document.querySelector(".decrease")
-const currentText = document.querySelector(".current")
+const increase = document.querySelectorAll(".increase")
+const decrease = document.querySelectorAll(".decrease")
+const currentText = document.querySelectorAll(".current")
 
-const itemAdd = document.querySelector(".item-add")
-const cartBody = document.querySelector(".cart-body")
+const itemAdd = document.querySelectorAll(".item-add")
+const cartBody = document.querySelectorAll(".cart-body")
 
 
 
 cartContainer.onclick = () => shoppingCart.classList.toggle('visable')
 
+
 // load main img by click
-thumbnailsImg.forEach(
-	function (item) {
-		item.onclick = () => {
-			productImg.src = item.src
-			galleryImg.src = item.src
-	}
-})
 
-// current thumbnail
 
-thumbnailsImg.forEach(function(img){
-	img.addEventListener("click", function(){
-		thumbnailsImg.forEach(function (item){
-			if(item.dataset.nr !== img.dataset.nr){
+eachProduct.forEach(function (item){
+	const itemThumbnailImg = item.querySelectorAll(".thumbnail-img")
+	const itemProductImg = item.querySelector(".product-img")
+	const itemGalleryImg = item.querySelector(".gallery-img")
+	const itemGalleryContainer = item.querySelector(".wrapper")
+	const itemCloseBtn = item.querySelector(".close-btn")
+	const itemNextBtn = item.querySelector(".next-btn")
+	const itemPreviusBtn = item.querySelector(".previus-btn")
+	const itemGalleryThumbnails= item.querySelectorAll(".gallery-thumbnails .thumbnail .thumbnail-img")
+	const itemThumbnailsImg = item.querySelectorAll(".thumbnail-img")
+	
+
+	itemThumbnailImg.forEach(
+		function (item) {
+			item.onclick = () => {
+				itemProductImg.src = item.src
+				itemGalleryImg.src = item.src
+		}
+	})
+	
+// show/hide gallery
+
+	itemProductImg.addEventListener("click", function(){
+		itemGalleryContainer.classList.add('visable')
+	})
+
+	itemThumbnailImg.forEach(function(img){
+		img.addEventListener("click", function(){
+			itemThumbnailImg.forEach(function (item){
+				if(item.dataset.nr !== img.dataset.nr){
+					item.classList.remove("opacity");
+					item.parentElement.classList.remove("active");
+				}
+			})
+			itemThumbnailImg.forEach(function (item){
+				if(item.dataset.nr == img.dataset.nr){
+					item.classList.add("opacity");
+					item.parentElement.classList.add("active");
+				}
+			})
+		})
+	})
+
+	itemCloseBtn.addEventListener("click",() => {
+			itemGalleryContainer.classList.remove('visable')
+		})
+	
+	document.addEventListener('keydown', (e)=>{
+		if(e.key === "Escape"){
+			itemGalleryContainer.classList.remove('visable')
+		}
+	})
+
+// next img
+let currentImg = 0;
+
+itemNextBtn.addEventListener("click", function () {
+		currentImg++
+		if (currentImg > itemGalleryThumbnails.length - 1) {
+			itemProductImg.src = itemGalleryThumbnails[0].src
+			itemGalleryImg.src = itemGalleryThumbnails[0].src
+			currentImg = 0
+		}
+		else {
+			itemProductImg.src= itemGalleryThumbnails[currentImg].src
+			itemGalleryImg.src = itemGalleryThumbnails[currentImg].src
+		}
+		itemThumbnailsImg.forEach(function (item){
+			if(item.dataset.nr !== itemThumbnailsImg[currentImg].dataset.nr){
 				item.classList.remove("opacity");
 				item.parentElement.classList.remove("active");
 			}
 		})
-		thumbnailsImg.forEach(function (item){
-			if(item.dataset.nr == img.dataset.nr){
+		itemThumbnailsImg.forEach(function (item){
+			if(item.dataset.nr == itemThumbnailsImg[currentImg].dataset.nr){
 				item.classList.add("opacity");
 				item.parentElement.classList.add("active");
 			}
 		})
-	})
+	  });
+
+	// previous img
+	itemPreviusBtn.addEventListener("click", function () {
+		currentImg--
+		if (currentImg < 0) {
+			itemProductImg.src = itemGalleryThumbnails[3].src
+			itemGalleryImg.src = itemGalleryThumbnails[3].src
+			currentImg = itemGalleryThumbnails.length - 1
+		}
+		else {
+			itemProductImg.src= itemGalleryThumbnails[currentImg].src
+			itemGalleryImg.src = itemGalleryThumbnails[currentImg].src
+		}
+		itemThumbnailsImg.forEach(function (item){
+			if(item.dataset.nr !== thumbnailsImg[currentImg].dataset.nr){
+				item.classList.remove("opacity");
+				item.parentElement.classList.remove("active");
+			}
+		})
+		itemThumbnailsImg.forEach(function (item){
+			if(item.dataset.nr == itemThumbnailsImg[currentImg].dataset.nr){
+				item.classList.add("opacity");
+				item.parentElement.classList.add("active");
+			}
+		})
+	});
+
+
+
+
+
+
 })
 
-// show/hide gallery
 
-productImg.onclick = () => galleryContainer.classList.add('visable')
-
-
-closeBtn.addEventListener("click",(e) => {
-	galleryContainer.classList.remove('visable')
-})
-
-document.addEventListener('keydown', (e)=>{
-	if(e.key=== "Escape"){
-	galleryContainer.classList.remove('visable')
-}
-})
-
-const to = document.querySelectorAll(".thumbnail")
-to.forEach(function(item)
-{
-	item.addEventListener("click", (e)=> {e.stopPropagation()})
-})
-
-// next img
-let currentItem = 0;
-
-nextBtn.addEventListener("click", function () {
-	currentItem++
-	if (currentItem > galleryThumbnails.length - 1) {
-		productImg.src = galleryThumbnails[0].src
-		galleryImg.src = galleryThumbnails[0].src
-		currentItem = 0
-	}
-	else {
-		productImg.src= galleryThumbnails[currentItem].src
-		galleryImg.src = galleryThumbnails[currentItem].src
-	}
-	thumbnailsImg.forEach(function (item){
-		if(item.dataset.nr !== thumbnailsImg[currentItem].dataset.nr){
-			item.classList.remove("opacity");
-			item.parentElement.classList.remove("active");
-		}
-	})
-	thumbnailsImg.forEach(function (item){
-		if(item.dataset.nr == thumbnailsImg[currentItem].dataset.nr){
-			item.classList.add("opacity");
-			item.parentElement.classList.add("active");
-		}
-	})
-  });
-
-// previous img
-previusBtn.addEventListener("click", function () {
-	currentImg--
-	if (currentItem < 0) {
-		productImg.src = galleryThumbnails[3].src
-		galleryImg.src = galleryThumbnails[3].src
-		currentImg = galleryThumbnails.length - 1
-	}
-	else {
-		productImg.src= galleryThumbnails[currentImg].src
-		galleryImg.src = galleryThumbnails[currentImg].src
-	}
-	thumbnailsImg.forEach(function (item){
-		if(item.dataset.nr !== thumbnailsImg[currentImg].dataset.nr){
-			item.classList.remove("opacity");
-			item.parentElement.classList.remove("active");
-		}
-	})
-	thumbnailsImg.forEach(function (item){
-		if(item.dataset.nr == thumbnailsImg[currentImg].dataset.nr){
-			item.classList.add("opacity");
-			item.parentElement.classList.add("active");
-		}
-	})
-  });
-
+//  it working but i could change it
 // counter
+currentText.forEach(function(item){
+	let counterItem = 0
+	increase.forEach(function(btn){
+		btn.addEventListener("click",()=>{
+		if(item.parentElement == btn.parentElement){
+			counterItem++,
+			item.innerHTML=counterItem}
+	})
+	})
+	decrease.forEach(function(btn){
+		btn.addEventListener("click", ()=> {
+			if(item.parentElement == btn.parentElement && counterItem > 0){
+				counterItem--;
+				item.innerHTML=counterItem;
+			}
+		})
+	})
 
-let counterItem = 0
+})
 
-increase.addEventListener("click", ()=> {counterItem++, currentText.innerHTML=counterItem})
-decrease.addEventListener("click", ()=> {
-	if(counterItem <=0){
-		 courentItem = 0
-		}
-	else{
-		counterItem--;
-		currentText.innerHTML=counterItem;
-	}})
 
 // //////////////////////////////////////////////////////////////////////
 // add to cart
 
-itemAdd.addEventListener("click", function(){
-	let price = (counterItem * 125)
-	if(price>0){
-		cartBody.innerHTML = 
-		`<div>
-			<div class="cart-product">
-				<img class="thumbnail-cart" src="./images/image-product-1-thumbnail.jpg" alt="product img">
-				<div class="product-cart-info">
-					<p>Fall Limited Edition Sneakers</p>
-					<p>$125.00 x ${counterItem} <b>$${price}.00</b> </p>
+itemAdd.forEach(function(item){
+	item.addEventListener("click", function(){
+		let price = (counterItem * 125)
+		if(price>0){
+			cartBody.innerHTML =
+			`<div>
+				<div class="cart-product">
+					<img class="thumbnail-cart" src="./images/image-product-1-thumbnail.jpg" alt="product img">
+					<div class="product-cart-info">
+						<p>Fall Limited Edition Sneakers</p>
+						<p>$125.00 x ${counterItem} <b>$${price}.00</b> </p>
+					</div>
+					<button class="delete-img"><svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg></button>
 				</div>
-				<button class="delete-img"><svg width="14" height="16" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M0 2.625V1.75C0 1.334.334 1 .75 1h3.5l.294-.584A.741.741 0 0 1 5.213 0h3.571a.75.75 0 0 1 .672.416L9.75 1h3.5c.416 0 .75.334.75.75v.875a.376.376 0 0 1-.375.375H.375A.376.376 0 0 1 0 2.625Zm13 1.75V14.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 1 14.5V4.375C1 4.169 1.169 4 1.375 4h11.25c.206 0 .375.169.375.375ZM4.5 6.5c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Zm3 0c0-.275-.225-.5-.5-.5s-.5.225-.5.5v7c0 .275.225.5.5.5s.5-.225.5-.5v-7Z" id="a"/></defs><use fill="#C3CAD9" fill-rule="nonzero" xlink:href="#a"/></svg></button>
-			</div>
-			<div class="summary-btn">
-				<button class="shop-list">Checkout</button>
-			</div>
-		</div>`;
-}
-		const deleteItem = document.querySelector(".delete-img")
-
-		deleteItem.addEventListener("click", function(){
-			cartBody.innerHTML = `<p>Your cart is empty.</p>`;
-		})
+				<div class="summary-btn">
+					<button class="shop-list">Checkout</button>
+				</div>
+			</div>`;
+	}
+			const deleteItem = document.querySelector(".delete-img")
+	
+			deleteItem.addEventListener("click", function(){
+				cartBody.innerHTML = `<p>Your cart is empty.</p>`;
+			})
+	})
 })
-
---
+++ products.js
@@ -0,0 +1,87 @@
+export let products = [
+	{
+		id: 1,
+		name: 'snekers shoes',
+		brand: 'SNEAKER COMPANY',
+		series: 'Fall Limited Edition Sneakers',
+		description: 'These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.',
+		price: 125,
+		incart:0,
+		photo: ["image-product-1.jpg","image-product-2.jpg","image-product-3.jpg","image-product-4.jpg"]
+	},
+	{
+		id:2,
+		name: 'snekers hoodie',
+		brand: 'SNEAKER COMPANY',
+		series: 'Fall Limited Edition Sneakers',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod optio asperiores aliquam fugit nemo vitae doloribus odio itaque tempore iure, nihil beatae quasi fugiat harum laborum error quia. Quia, cumque?',
+		price: 50,
+		incart:0,
+		photo: ["hoodie-product-1.jpg","hoodie-product-2.jpg","hoodie-product-3.jpg","hoodie-product-4.jpg"]
+	}
+]
+
+export function displayProducts(products){
+	let displayProduct = products.map(function(item){
+
+		return`<section class="product">
+		<div class="lightbox">
+		  <button class="img-btn">
+			<img class="product-img" src="./images/${item.photo[0]}" alt="main img">
+		  </button>
+		  <ul class="thumbnails">
+		  <li class="thumbnail active"><img class="thumbnail-img opacity" data-nr="${item.id}1" src="./images/${item.photo[0]}" alt="1 thumbanil"></li>
+			<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}2" src="./images/${item.photo[1]}" alt="2 thumbanil" ></li>
+			<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}3" src="./images/${item.photo[2]}" alt="3 thumbanil"></li>
+			<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}4" src="./images/${item.photo[3]}" alt="4 thumbanil"></li>
+		  </ul>
+		</div>
+		<div class="wrapper">
+		  <div class="gallery-container">
+			<div class="close-gallery">
+			  <button class="close-btn">
+				<svg class="close-icon" width="14" height="15" xmlns="http://www.w3.org/2000/svg"><path d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z" fill="#69707D" fill-rule="evenodd"/></svg>
+			  </button>
+			</div>
+			<div class="main-container">
+			  <button class="previus-btn">
+				<svg class="previus-icon" width="12" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 1 3 9l8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+			  </button>
+			  <img class="gallery-img" src="./images/${item.photo[0]}" alt="main img">
+			  <button class="next-btn">
+				<svg class="next-icon" width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+			  </button>
+			</div>
+			  <ul class="gallery-thumbnails">
+			  <li class="thumbnail active"><img class="thumbnail-img opacity" data-nr="${item.id}1" src="./images/${item.photo[0]}" alt="1 thumbanil"></li>
+				<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}2" src="./images/${item.photo[1]}" alt="2 thumbanil" ></li>
+				<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}3" src="./images/${item.photo[2]}" alt="3 thumbanil"></li>
+				<li class="thumbnail"><img class="thumbnail-img" data-nr="${item.id}4" src="./images/${item.photo[3]}" alt="4 thumbanil"></li>
+			  </ul>
+		  </div>
+		</div>
+		<article class="description">
+		  <div class="product__info">
+			<p class="brand">${item.brand}</p>
+			<h1>${item.series}</h1>
+			<p class="description-info">
+			  ${item.description}
+			</p>
+			<div class="price">
+			  <p class="discount-price">$${item.price}</p>
+		  </div>
+		  <div class="item">
+			<div class="counter">
+			  <button class="decrease segment"><svg width="12" height="4" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M11.357 3.332A.641.641 0 0 0 12 2.69V.643A.641.641 0 0 0 11.357 0H.643A.641.641 0 0 0 0 .643v2.046c0 .357.287.643.643.643h10.714Z" id="a"/></defs><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#a"/></svg></button>
+			  <p class="current segment">0</p>
+			  <button class="increase segment"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><path d="M12 7.023V4.977a.641.641 0 0 0-.643-.643h-3.69V.643A.641.641 0 0 0 7.022 0H4.977a.641.641 0 0 0-.643.643v3.69H.643A.641.641 0 0 0 0 4.978v2.046c0 .356.287.643.643.643h3.69v3.691c0 .356.288.643.644.643h2.046a.641.641 0 0 0 .643-.643v-3.69h3.691A.641.641 0 0 0 12 7.022Z" id="b"/></defs><use fill="#FF7E1B" fill-rule="nonzero" xlink:href="#b"/></svg></button>
+			</div>
+			<button class="item-add">
+			  <svg class="cart" width="22" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill-rule="nonzero"/></svg>
+			  <p>Add to cart</p></button>
+			</div>
+		</article>
+	  </section>`
+	})
+	return displayProduct = displayProduct.join("")
+}
